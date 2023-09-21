8d78388b2de656e4e07cd06505c084ade275adab
hgulersen <haymigulersen@gmail.com>
2023-09-21 16:42:15 +0200

    more docs

--- api/feed.ts
+++ api/feed.ts
@@ -2,7 +2,10 @@ import axios from "axios";
 import config from "../config";
 import { prefetchImages } from "../utils";
 
-// getOneQuestion returns a single question from the API
+/**
+ * Fetches a single question from the For You API.
+ * @returns A Promise that resolves to a ForYouApiResponse object containing the question data, or an error object if the request fails.
+ */
 export const fetchOneQuestion = async (): Promise<ForYouApiResponse> => {
   try {
     const response = await axios.get(
@@ -18,6 +21,12 @@ export const fetchOneQuestion = async (): Promise<ForYouApiResponse> => {
   }
 };
 
+/**
+ * Fetches n number of questions in parallel.
+ * @param n - The number of questions to fetch.
+ * @param shouldPrefetchImages - Whether or not to prefetch images.
+ * @returns A Promise that resolves to an array of Question objects.
+ */
 export const fetchNQuestionsInParallel = async (
   n: number,
   shouldPrefetchImages: boolean = false
@@ -38,6 +47,11 @@ export const fetchNQuestionsInParallel = async (
   return data;
 };
 
+/**
+ * Retrieves an answer from the API based on the provided question ID.
+ * @param questionId The ID of the question to retrieve the answer for.
+ * @returns A Promise that resolves to an object containing the answer data or an error object if the request fails.
+ */
 export const getAnswer = async (
   questionId: number
 ): Promise<AnswerApiResponse> => {
